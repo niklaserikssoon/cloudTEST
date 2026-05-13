614a0130322d0a2143ae8737fc374cc40f98ab21
niklas eriksson <niklaseriksson32@hotmail.com>
2026-05-13 12:10:07 +0200

    Add project files.

---
+++ CloudNativveApp.Api/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudNativeApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StatusController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public StatusController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("health")]
+    public IActionResult GetHealth()
+    {
+        return Ok(new { Status = "Healthy", Timestamp = DateTime.UtcNow });
+    }
+
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var secretValue = _configuration["AppSecret"];
+
+        if (string.IsNullOrEmpty(secretValue))
+        {
+            return NotFound("Ingen hemlighet hittades.");
+        }
+
+        return Ok(new { SecretMessage = secretValue });
+    }
+}
